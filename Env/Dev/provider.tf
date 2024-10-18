982b321162a354606e40045239b7d2faee6d504b
Kalpna Patel <167253909+kalpna-patel@users.noreply.github.com>
2024-10-18 13:31:35 +0530

    Update provider.tf

--- Env/Dev/provider.tf
+++ Env/Dev/provider.tf
@@ -2,11 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.103.1"
+      version = "4.5.0"
     }
+    }
+     backend "azurerm" {
+      resource_group_name  = "kp-rg"
+      storage_account_name = "githubactionstr"
+      container_name       = "cariadcnt"
+      key                  = "cariad.tfstate"
   }
 }
-
 provider "azurerm" {
-features{}
+  features {}
 }
